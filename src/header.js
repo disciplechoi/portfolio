f383b1989c4fa060433b1809e1481774e003f735
Jihye Soriano <jihyesoriano@jihyes-mbp.mynetworksettings.com>
2024-02-09 16:21:39 -0500

    first commit

---
+++ src/header.js
@@ -0,0 +1,21 @@
+export default function Header(){
+    return (
+        <>
+            <header>
+            <p>PORTPOLIO</p>
+            <h1>Jihye Soriano</h1>
+            <h2>Full Stack Developer</h2>
+            
+            <ul>
+            <li>Email : dddchoijam1229@gmail.com</li>
+            </ul>
+
+            <ul>
+            <li>linkdin</li>
+            </ul>
+             </header>
+        </>
+        
+    )
+
+}

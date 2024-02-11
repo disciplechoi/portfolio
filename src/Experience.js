399ed9853011a7159bdc5661032f06c870910dff
Jihye Soriano <jihyesoriano@jihyes-mbp.mynetworksettings.com>
2024-02-10 20:32:05 -0500

    header animation

--- src/Experience.js
+++ src/Experience.js
@@ -6,15 +6,33 @@ export default function Experience(){
     <div className="experience text-left pt-10">
         <h1 className="font-bold text-3xl">Experience</h1>
 
-        <ul>
-            <h2 className="text-xl">Samsung SDS America</h2>
-            <h3 className="text-xl">ServiceNow Consultant</h3>
-            <li> Administered and maintained the ServiceNow platform, ensuring optimal performance, availability, and security.</li>
+        <h2 className="text-xl">Samsung SDS America</h2>
+        <h3 className="text-xl">ServiceNow Consultant</h3>
+        <ul>   
+            <li>Administered and maintained the ServiceNow platform, ensuring optimal performance, availability, and security.</li>
+            <li>Provided end-user support and training to enhance ServiceNow adoption across the organization. Implemented and managed integrations with third-party tools, ensuring seamless data flow and process automation.</li>
+            <li>Collaborated with developers to design and implement custom applications on the ServiceNow platform.</li>
+            <li>Assisted in the development of ServiceNow best practices documentation and training materials.</li>
         </ul>
        
 
-        <h2 className="text-xl">Universal Steel Products</h2>
-        <h2 className="text-xl">DGB Data System</h2>
+        <h2 className="text-xl pt-5">Universal Steel Products</h2>
+        <h3 className="text-xl">Full Stack Developer</h3>
+        <ul>   
+            <li>Administered and maintained the ServiceNow platform, ensuring optimal performance, availability, and security.</li>
+            <li>Provided end-user support and training to enhance ServiceNow adoption across the organization. Implemented and managed integrations with third-party tools, ensuring seamless data flow and process automation.</li>
+            <li>Collaborated with developers to design and implement custom applications on the ServiceNow platform.</li>
+            <li>Assisted in the development of ServiceNow best practices documentation and training materials.</li>
+        </ul>
+
+        <h2 className="text-xl pt-5">DGB Data System</h2>
+        <h3 className="text-xl">Full Stack Developer</h3>
+        <ul>   
+            <li>Administered and maintained the ServiceNow platform, ensuring optimal performance, availability, and security.</li>
+            <li>Provided end-user support and training to enhance ServiceNow adoption across the organization. Implemented and managed integrations with third-party tools, ensuring seamless data flow and process automation.</li>
+            <li>Collaborated with developers to design and implement custom applications on the ServiceNow platform.</li>
+            <li>Assisted in the development of ServiceNow best practices documentation and training materials.</li>
+        </ul>
     </div>
     
     </>
--- src/header.js
+++ src/header.js
@@ -5,14 +5,18 @@ export default function Header(){
         <>
             <header className='text-left'>
                 <p className="font-bold">PORTPOLIO</p>
-            <div className="pt-16">
-                <img className="selfie" src={selfie} alt="selfie" />
-                <div className="text-3xl pt-5">
-                    <h1 className="font-bold ">Jihye Soriano</h1>
-                    <h2>Full Stack Developer</h2>
+            <div className="pt-16 fade-in-box ">
+                <div>
+                    <img className="selfie" src={selfie} alt="selfie" />
+                    
                 </div>
+               
                 
                 <div className='pt-5'>
+                    <div className="text-3xl pt-5">
+                        <h1 className="font-bold ">Jihye Soriano</h1>
+                        <h2>Full Stack Developer</h2>
+                    </div>
                     <ul>
                     <li>Contact : choijam1229@gmail.com</li>
                     </ul>

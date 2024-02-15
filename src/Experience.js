e19fe0136c76b6aab38b6829fd6a4a5a2d3525a7
Jihye Soriano <jihyesoriano@jihyes-mbp.mynetworksettings.com>
2024-02-14 19:24:20 -0500

    skills

--- src/Experience.js
+++ src/Experience.js
@@ -9,9 +9,17 @@ export default function Experience(){
         <div className="experience-card">
             <div className="job-title flex">
               <h2 className="text-xl font-bold">Samsung SDS America | </h2>
-              <h3 className="text-xl"> ServiceNow Consultant(BA)</h3>
+              <h3 className="text-xl"> Business Analyst</h3>
             </div>
-            
+             Skills : <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Project Management</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Communication</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Documentation</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Reporting</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Requirements Elicitation</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Quality Assurance</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">ServiceNow Cloud</button>
+              {/* <p>Skills  : ServiceNow Cloud Web Application Operation</p> */}
+
             <ul>   
                 <li>Administered and maintained the ServiceNow platform, ensuring optimal performance, availability, and security.</li>
                 <li>Provided end-user support and training to enhance ServiceNow adoption across the organization. Implemented and managed integrations with third-party tools, ensuring seamless data flow and process automation.</li>
@@ -25,6 +33,14 @@ export default function Experience(){
           <h2 className="text-xl font-bold">Universal Steel Products | </h2>
           <h3 className="text-xl">Full Stack Developer</h3>
         </div>
+          Skills : <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">React</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Next.js</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">HTML/CSS</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Javascript</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Tailwind</button>
+             {/* <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1">Quality Assurance</button> */}
+             {/* <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1">ServiceNow Cloud</button> */}
+          {/* <p>Skills  : ServiceNow Cloud Platform Operation</p> */}
         <ul>   
             <li>Logistic web application development using React, Nex.js and Prisma DB</li>
             <li>Designed dynamic and browser compatible pages using HTML5, CSS3, Tailwind</li>
@@ -38,6 +54,9 @@ export default function Experience(){
           <h2 className="text-xl font-bold">DGB Data System | </h2>
           <h3 className="text-xl">SQL Developer</h3>
         </div>
+        Skills : <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1 mr-1">Oracle SQL</button>
+             <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1">Banking System</button>
+             {/* <button class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full mb-1">HTML/CSS</button> */}
        
         <ul>   
             <li>Financial services web application/Oracle DB Management</li>
